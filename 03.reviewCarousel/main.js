1db94db23c99a16c47f976600fad08fdb9b01ea4
hungerbk <wjantlr@gmail.com>
2023-07-13 23:07:00 +0900

    [추가] 리뷰 객체 생성 - 프로필 이미지 파일 추가

---
+++ 03.reviewCarousel/main.js
@@ -0,0 +1,42 @@
+const reviewList = [
+  {
+    id: 1,
+    name: "짱난",
+    job: "유치원생 탐정",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis nulla culpa nam, ad nesciunt quo maiores, sint dolor id tempora dolorem minus! Quia, molestias nobis accusamus vel officiis sapiente eos.",
+    profileImage: "./profile1.jpeg",
+  },
+  {
+    id: 2,
+    name: "흰둥이",
+    job: "강아지 유치원생",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis nulla culpa nam, ad nesciunt quo maiores, sint dolor id tempora dolorem minus! Quia, molestias nobis accusamus vel officiis sapiente eos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis nulla culpa nam, ad nesciunt quo maiores, sint dolor id tempora dolorem minus! Quia, molestias nobis accusamus vel officiis sapiente eos.",
+    profileImage: "./profile2.jpeg",
+  },
+  {
+    id: 3,
+    name: "짱아",
+    job: "고등학생",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis nulla culpa nam, ad nesciunt quo maiores, sint dolor id tempora dolorem minus! Quia, molestias nobis accusamus vel officiis sapiente eos.",
+    profileImage: "./profile3.jpeg",
+  },
+  {
+    id: 4,
+    name: "신형만",
+    job: "로봇",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis nulla culpa nam, ad nesciunt quo maiores, sint dolor id tempora dolorem minus! Quia, molestias nobis accusamus vel officiis sapiente eos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis nulla culpa nam, ad nesciunt quo maiores, sint dolor id tempora dolorem minus! Quia, molestias nobis accusamus vel officiis sapiente eos.",
+    profileImage: "./profile4.jpeg",
+  },
+  {
+    id: 5,
+    name: "봉미선",
+    job: "짱쎈신부",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis nulla culpa nam, ad nesciunt quo maiores, sint dolor id tempora dolorem minus! Quia, molestias nobis accusamus vel officiis sapiente eos.",
+    profileImage: "./profile5.png",
+  },
+];
