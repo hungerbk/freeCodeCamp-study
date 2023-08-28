6803be2324f25655bbd1623dc52ea13cc1a826cc
hungerbk <wjantlr@gmail.com>
2023-08-28 17:57:42 +0900

    [수정] 클릭 이벤트 수정
    - 버튼 클릭 시 클릭한 버튼에 해당하는 디테일만 보이도록 if문 추가
    
    - 버튼에 forEach를 지정하는 것이 아니라 question item 내부 항목 선택하여 forEach 적용하도록 수정

--- 07.faqPage/main.js
+++ 07.faqPage/main.js
@@ -1,17 +1,30 @@
-// document.querySelectorAll()
-// addEventListener()
-// forEach()
-// classList.remove()
-// classList.toggle()
+// 한 요소를 선택 후, 그 요소의 내부 요소를 선택하여 이벤트를 적용하는 방식
 
-// 클릭 이벤트가 발생하면 모든 디테일에서 show 클래스를 지운 후, 클릭한 버튼에 해당하는 디테일만 show를 남겨둠..?
+const questions = document.querySelectorAll(".question-item");
 
-const detailBtns = document.querySelectorAll(".show-detail-btn");
-
-detailBtns.forEach((btn) => {
-  btn.addEventListener("click", function (e) {
-    const detail = e.currentTarget.parentElement.children[2];
+questions.forEach((question) => {
+  const detailBtn = question.querySelector(".show-detail-btn");
+  const detail = question.querySelector(".detail");
+  detailBtn.addEventListener("click", function () {
+    questions.forEach((item) => {
+      if (item.children[2] !== detail && item.children[1] !== detailBtn) {
+        item.children[2].classList.remove("show-text");
+        item.children[1].classList.remove("show-text");
+      }
+    });
     detail.classList.toggle("show-text");
-    e.currentTarget.classList.toggle("show-text");
+    detailBtn.classList.toggle("show-text");
   });
 });
+
+// 필요한 요소를 선택하여 이벤트를 지정하는 방식
+
+// const detailBtns = document.querySelectorAll(".show-detail-btn");
+
+// detailBtns.forEach((btn) => {
+//   btn.addEventListener("click", function (e) {
+//     const detail = e.currentTarget.parentElement.children[2];
+//     detail.classList.toggle("show-text");
+//     e.currentTarget.classList.toggle("show-text");
+//   });
+// });
