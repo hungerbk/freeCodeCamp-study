a2d556e4b8bafed0ce92c16377459d78c34fed04
hungerbk <wjantlr@gmail.com>
2023-08-29 23:43:20 +0900

    [추가] 메뉴 객체 리스트 생성
    - 이미지 수정 필요

--- 08.menu/main.js
+++ 08.menu/main.js
@@ -5,3 +5,69 @@
 // map, reduce, filter
 // innerHTML
 // includes 메서드
+const menu = [
+  {
+    id: 1,
+    title: "buttermlik pancakes",
+    category: "breakfast",
+    price: 15.99,
+    img: "../03.reviewCarousel/profile1.jpeg",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ratione eos nemo ipsa adipisci porro, aperiam blanditiis sint repellendus tenetur est eligendi autem sit pariatur praesentium sunt iste nostrum ex?",
+  },
+  {
+    id: 2,
+    title: "diner double",
+    category: "lunch",
+    price: 13.99,
+    img: "../03.reviewCarousel/profile1.jpeg",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta a ad nobis vitae ipsa deleniti veritatis accusamus magni minus quae.",
+  },
+  {
+    id: 3,
+    title: "godzilla milkshake",
+    category: "shakes",
+    price: 6.99,
+    img: "../03.reviewCarousel/profile1.jpeg",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error eaque esse facilis vero fuga nostrum dolores velit, libero pariatur, sed voluptates animi, totam a architecto.",
+  },
+  {
+    id: 4,
+    title: "country delight",
+    category: "breakfast",
+    price: 20.99,
+    img: "../03.reviewCarousel/profile1.jpeg",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, minus? Cumque vitae voluptate saepe fugit numquam amet iste in soluta doloremque quam hic quia, tenetur corrupti dolorem quae. Nihil, distinctio.",
+  },
+  {
+    id: 5,
+    title: "egg attack",
+    category: "lunch",
+    price: 15.99,
+    img: "../03.reviewCarousel/profile1.jpeg",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat recusandae quisquam voluptates ut in cum dignissimos obcaecati doloremque fuga. Perferendis et placeat adipisci nihil, cumque, labore cupiditate ipsum sit repudiandae fugiat sequi perspiciatis incidunt veniam veritatis tempore eius sint reprehenderit.",
+  },
+  {
+    id: 6,
+    title: "oreo dream",
+    category: "shakes",
+    price: 18.99,
+    img: "../03.reviewCarousel/profile1.jpeg",
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam in quidem, saepe possimus cupiditate facilis nobis ea magnam! Saepe, vel est. Eaque provident suscipit voluptatibus, molestiae eum sapiente aliquid at magnam itaque nihil.",
+  },
+  {
+    id: 7,
+    title: "bacon overflow",
+    category: "breakfast",
+    price: 8.99,
+    img: "../03.reviewCarousel/profile1.jpeg",
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione rem similique laborum laudantium! Ut hic alias dolorum nostrum quisquam?",
+  },
+  {
+    id: 8,
+    title: "america classic",
+    category: "lunch",
+    price: 12.99,
+    img: "../03.reviewCarousel/profile1.jpeg",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis veritatis iusto voluptas voluptates culpa odio tenetur enim ipsa quidem modi quasi, dicta at nemo expedita laboriosam hic.",
+  },
+];
