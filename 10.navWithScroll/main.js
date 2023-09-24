e103d0503f51b29e3a2f9f4a20d1a70e07bf4125
hungerbk <wjantlr@gmail.com>
2023-09-24 23:59:16 +0900

    [수정] 모바일 링크 리스트 동적 생성

--- 10.navWithScroll/main.js
+++ 10.navWithScroll/main.js
@@ -1,12 +1,21 @@
 const navBtn = document.querySelector(".nav-button");
 const lisnksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+const nav = document.getElementById("nav");
 
 navBtn.addEventListener("click", function () {
-  lisnksContainer.classList.toggle("show-links");
-  nav.classList.toggle("show-links");
+  // lisnksContainer.classList.toggle("show-links");
+  // nav.classList.toggle("show-links");
+  const containerHeigt = lisnksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+
+  if (containerHeigt === 0) {
+    lisnksContainer.style.height = `${linksHeight}px`;
+  } else {
+    lisnksContainer.style.height = 0;
+  }
 });
 
 // set date
-const nav = document.getElementById("nav");
 const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
