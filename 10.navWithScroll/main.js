575c31c7b77fa62e4db742ba26f6f67615569cad
hungerbk <wjantlr@gmail.com>
2023-09-26 16:11:09 +0900

    [수정] 모바일 링크 리스트 동적 생성 스타일 수정
    - nav 배경색을 클릭하지 않아도 흰색으로 보이게 수정
    - 변경한 links와 links-container에 맞게 스타일 수정
    - 동적으로 생성하는 경우, 모바일에서 리스트를 안보이게 한 뒤 데스크탑으로 바꾸면 링크리스트가 안보이는 문제 발생
    -> !important를 사용하여 문제 해결. 하지만 !important 사용은 지양하라고 했는데,
    다른 방법은 없는지.. 없다면 동적 생성을 안하는 것이 낫지 않은지 고민..

--- 10.navWithScroll/main.js
+++ 10.navWithScroll/main.js
@@ -1,17 +1,18 @@
+// 모바일 링크 리스트 동적 생성
 const navBtn = document.querySelector(".nav-button");
 const lisnksContainer = document.querySelector(".links-container");
 const links = document.querySelector(".links");
-const nav = document.getElementById("nav");
 
 navBtn.addEventListener("click", function () {
-  // lisnksContainer.classList.toggle("show-links");
-  nav.classList.toggle("show-links");
   const containerHeigt = lisnksContainer.getBoundingClientRect().height;
   const linksHeight = links.getBoundingClientRect().height;
 
+  // 클릭했을 때 현재 높이가 0이면 (리스트가 안보이는 상태)
   if (containerHeigt === 0) {
+    // 리스트를 보여줌 (리스트의 높이만큼)
     lisnksContainer.style.height = `${linksHeight}px`;
   } else {
+    // 높이가 0이 아니면(리스트가 보이는 상태) 리스트를 안보이게 바꿈
     lisnksContainer.style.height = 0;
   }
 });
