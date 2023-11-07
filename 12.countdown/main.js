100ee3ec527695f08a5876185a518b88bde04974
hungerbk <wjantlr@gmail.com>
2023-11-08 00:17:19 +0900

    [추가] 완료 날짜 동적 생성 - 적용되고 있었는데 모르고 있던 거였다..

--- 12.countdown/main.js
+++ 12.countdown/main.js
@@ -1,17 +1,23 @@
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-const Weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 
-let futureDate = new Date(2023, 12, 24, 11, 30, 0);
+let futureDate = new Date(2023, 11, 25, 11, 30, 0);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
 const minutes = futureDate.getMinutes();
 
-giveaway.textContent = `Giveaway Ends on Sunday, 24 April ${year} ${hours}: ${minutes} am`;
+let month = futureDate.getMonth();
+month = months[month];
 
-console.log(futureDate);
+const date = futureDate.getDate();
+const weekday = weekdays[futureDate.getDay()];
+
+giveaway.textContent = `Giveaway Ends on ${weekday}, ${date} ${month} ${year} ${hours}: ${minutes} am`;
+
+console.log(months[month]);
