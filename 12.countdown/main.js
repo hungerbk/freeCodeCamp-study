d94c6209de105518ea0250872a1b2ee8a4f975c7
hungerbk <wjantlr@gmail.com>
2023-11-07 23:57:46 +0900

    [추가] 날짜 및 시간 동적 생성 작업 - textContent에 반영되지 않는 문제

---
+++ 12.countdown/main.js
@@ -0,0 +1,17 @@
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+const Weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+let futureDate = new Date(2023, 12, 24, 11, 30, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+giveaway.textContent = `Giveaway Ends on Sunday, 24 April ${year} ${hours}: ${minutes} am`;
+
+console.log(futureDate);
