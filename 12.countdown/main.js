bdf10875f82c5fdbfbff81d9bb87545c194620c1
hungerbk <wjantlr@gmail.com>
2023-11-08 02:21:08 +0900

    [추가] 카운트다운 생성 - 현재 시간과 데드라인을 비교해서 시간 설정 - setInterval()을 통해 1초마다 함수 실행하여 렌더링 - 데드라인을 지난 경우 예외처리

--- 12.countdown/main.js
+++ 12.countdown/main.js
@@ -6,6 +6,8 @@ const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 
+// month는 0부터 시작하는 인덱스 값
+// 시간은 24시간으로 0~23
 let futureDate = new Date(2023, 11, 25, 11, 30, 0);
 
 const year = futureDate.getFullYear();
@@ -19,3 +21,50 @@ const date = futureDate.getDate();
 const weekday = weekdays[futureDate.getDay()];
 
 giveaway.textContent = `Giveaway Ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes} am`;
+
+// future time in ms
+const futureTime = futureDate.getTime();
+
+function geteRmainingTime() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  // 1s = 1000ms
+  // 1m = 60s
+  // 1h = 60m
+  // 1d = 24h
+
+  // values in ms
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+
+  // calculate all values
+  let days = Math.floor(t / oneDay);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+
+  // set values in array
+  const values = [days, hours, minutes, seconds];
+
+  function format(item) {
+    if (item < 10) {
+      return (item = `0${item}`);
+    }
+    return item;
+  }
+
+  items.forEach(function (item, index) {
+    item.innerHTML = format(values[index]);
+  });
+
+  // 현재 시간이 deadline을 지난 경우
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="expired">Sorry, this giveaway has expired</h4>`;
+  }
+}
+
+// countdown
+let countdown = setInterval(geteRmainingTime, 1000);
+geteRmainingTime();
